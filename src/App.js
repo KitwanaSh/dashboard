e9859af38a8887a5a6680d7e31bdabeaf43978fe
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-06-06 13:17:21 +0200

    Fix: Update the components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter, Routes, Route} from  'react-router-dom'
-import { FiSettings } from 'react-icons/fi'
-import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route} from  'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Kanban, Customers, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from "./pages";
+
 
 import './App.css'
 const App = () => {
@@ -22,18 +26,18 @@ const App = () => {
           </div>
           {activeMenue ? (
             <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
-              SideBar
+              <Sidebar />
             </div>
           ) : (
             <div className='w-0 dark:bg-secondary-dark-bg'>
-              SideBar
+              <Sidebar />
             </div>
           )}
           <div className={
             `dark:bg-main-bg bg-main-bg w-full min-h-screen ${activeMenue ? 'md:ml-72' : 'flex-2'}`
           }>
             <div className='fixed md:static bg-main-bg dark:b-main-dark-bg navbar w-full'>
-              navbar
+              <Navbar />
             </div>
 
           </div>
