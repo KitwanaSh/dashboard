79b58bd0355d77b434e06a6e78952bcc76148216
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-06-15 13:51:19 +0200

    Feat: Start the Pyramid chart

--- src/pages/Charts/Pyramid.jsx
+++ src/pages/Charts/Pyramid.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, Inject, AccumulationDataLabel, AccumulationTooltip, AccumulationSelection, PyramidSeries, AccumulationLegend } from "@syncfusion/ej2-react-charts";
+
+import { Header } from '../../components';
+import { PyramidData } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const Pyramid = () => {
   return (
-    <div>Pyramid</div>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Pie" title="Sales Information" />
+      <div className='w-full'>
+        <AccumulationChartComponent>
+          <Inject services={[PyramidSeries, AccumulationDataLabel, AccumulationLegend, AccumulationSelection, AccumulationSelection, AccumulationTooltip]} />
+        </AccumulationChartComponent>
+      </div>
+    </div>
   )
 }
 
